1f5daaa8ed98250f0f599b3afb2bbba85daf47d0
yamadagennji <waynezhiyong@gmail.com>
2017-01-23 14:21:03 -0500

    Update re.findall.py

--- re.findall.py
+++ re.findall.py
@@ -14,3 +14,21 @@ import re
 x= 'From: Using the : character'
 y=re.findall('^F.+?:',x)
 print (y)
+
+
+x='From stephen.marquard@uct.ac.za Sat Jan 5 09:13:16 2008'
+import re
+y=re.findall('\S+@\S+',x)
+print (y)
+
+x='From stephen.marquard@uct.ac.za Sat Jan 5 09:13:16 2008'
+import re
+y=re.findall('^From (\S+@\S+)',x)
+print (y)
+
+x='From 222 stephen.marquard@uct.ac.za Sat Jan 5 09:13:16 2008'
+import re
+y=re.findall('^From.* (\S+@\S+)',x)
+print (y)
+
+#当需要字符而不是通配符输入时前面加 "\"
