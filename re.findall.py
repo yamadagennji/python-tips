f1084e893850191f222e656de012ca9633de50b9
yamadagennji <waynezhiyong@gmail.com>
2017-01-23 13:59:26 -0500

    Update re.findall.py

--- re.findall.py
+++ re.findall.py
@@ -2,3 +2,15 @@ import re
 x= 'My 2 favorate numbers are 19 and 42'
 y=re.findall('[aeiou]+',x)
 print (y)
+
+
+import re
+x= 'From: Using the : character'
+y=re.findall('^F.+:',x)
+print (y)
+
+
+import re
+x= 'From: Using the : character'
+y=re.findall('^F.+?:',x)
+print (y)
