40533e7a500b495719203fef676df0de57b3720b
yamadagennji <waynezhiyong@gmail.com>
2017-01-23 15:49:16 -0500

    Update urlib1.py

--- urlib1.py
+++ urlib1.py
@@ -3,3 +3,14 @@ fhand = urllib.request.urlopen('http://www.py4inf.com/code/romeo.txt')
 
 for line in fhand:
     print (line.strip())
+
+    
+import urllib.request
+fhand = urllib.request.urlopen('http://www.py4inf.com/code/romeo.txt')
+
+counts=dict()
+for line in fhand:
+    words=line.split()
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+print(counts)
